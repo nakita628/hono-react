1afecf7193ea4df338b94262b87e6718b149e6f2
nakita628 <mini.bg.pro.takibi.n@gmail.com>
2026-03-07 02:23:17 +0000

    add

--- apps/hono-react-cloudflare/src/api/index.ts
+++ apps/hono-react-cloudflare/src/api/index.ts
@@ -1,4 +1,7 @@
+import { swaggerUI } from '@hono/swagger-ui'
 import { createRoute, OpenAPIHono, type RouteHandler, z } from '@hono/zod-openapi'
+import { Scalar } from '@scalar/hono-api-reference'
+import { createMarkdownFromOpenApi } from '@scalar/openapi-to-markdown'
 
 export const app = new OpenAPIHono()
 
@@ -24,7 +27,28 @@ const getRouteHandler: RouteHandler<typeof getRoute> = async (c) => {
 export const api = app.basePath('/api').openapi(getRoute, getRouteHandler)
 
 if (import.meta.env?.DEV ?? process.env.NODE_ENV !== 'production') {
-  
+  api.doc('/doc', {
+    info: {
+      title: 'Hono Reac Cloudflare',
+      version: '1.0.0',
+    },
+    openapi: '3.1.0',
+  })
+
+  app.get('api/ui', swaggerUI({ url: '/api/doc' }))
+
+  app.get('api/scalar', Scalar({ url: '/api/doc' }))
+
+  const content = app.getOpenAPI31Document({
+    openapi: '3.1.0',
+    info: {
+      title: 'Hono React Cloudflare',
+      version: '1.0.0',
+    },
+  })
+
+  const markdown = await createMarkdownFromOpenApi(JSON.stringify(content))
+  app.get('api/llms.txt', (c) => c.text(markdown))
 }
 
 export default app
