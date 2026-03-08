1c3196ef4340dc1d2d951fabc504fbde05b71240
nakita628 <takibilabdev@gmail.com>
2026-03-08 01:22:12 +0000

    client fix

--- apps/hono-vinext-cloudflare/src/lib/client.ts
+++ apps/hono-vinext-cloudflare/src/lib/client.ts
@@ -2,8 +2,8 @@ import { hc } from 'hono/client'
 
 import type { api } from '@/backend'
 
-export type Client = ReturnType<typeof hc<typeof api>>
+type Client = ReturnType<typeof hc<typeof api>>
 
-export const hcWithType = (...args: Parameters<typeof hc>): Client => hc<typeof api>(...args)
+const hcWithType = (...args: Parameters<typeof hc>): Client => hc<typeof api>(...args)
 
 export const client = hcWithType('/').api
